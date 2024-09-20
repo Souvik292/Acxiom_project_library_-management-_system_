1ca25cde4c7710d1103cf9566ca0b1001aa13e62
SOUVIK BHOWMICK <160013879+Souvik292@users.noreply.github.com>
2024-09-20 22:16:28 +0530

    Update maintainance.js

--- public/javascript/maintainance.js
+++ public/javascript/maintainance.js
@@ -1,1 +1,21 @@
+function showModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
 
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    const membershipModal = document.getElementById('membershipModal');
+    const updateModal = document.getElementById('updateModal');
+    const bookMovieModal = document.getElementById('bookMovieModal');
+    if (event.target == membershipModal) {
+        membershipModal.style.display = "none";
+    } else if (event.target == updateModal) {
+        updateModal.style.display = "none";
+    } else if (event.target == bookMovieModal) {
+        bookMovieModal.style.display = "none";
+    }
+}
